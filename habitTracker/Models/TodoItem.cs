4a9f38acdd74ba24100d76adbc18fc65f20a5938
John Cuviello <jcuviell@uci.edu>
2024-03-19 17:16:42 -0700

    added basic setup for todo list component

---
+++ habitTracker/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿namespace habitTracker;
+
+public class TodoItem
+{
+    public TodoItem(string title)
+    {
+        Title = title;
+        IsCompleted = false;
+        IsPrivate = false;
+    }
+
+    public string Title { get; set; }
+    public bool IsCompleted { get; set; }
+    private bool IsPrivate { get; set; }
+
+}
